8087bc32594aeea8206200767e0c01c435141366
Anders Carlsson <awcarlsson1@gmail.com>
2020-03-15 16:55:03 -0500

    Added .env functionality and began cloud storage

--- naturedroid.py
+++ naturedroid.py
@@ -1,24 +1,33 @@
 import random
 import math
 import os
+from dotenv import load_dotenv
 from moviepy.editor import *
 from twython import Twython
+from google.cloud import storage
 
-#APP_KEY = ''
-#APP_SECRET = ''
-#OAUTH_TOKEN = ''
-#OAUTH_SECRET = ''
-#ACCOUNT_ID = ''
+load_dotenv()
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
+AP_KY = os.getenv('APP_KEY')
+AP_SEC = os.getenv('APP_SECRET')
+AUTH_TOK = os.getenv('OAUTH_TOKEN')
+AUTH_SEC = os.getenv('OAUTH_SECRET')
+ACNT_ID = os.getenv('ACCOUNT_ID')
+CL_JSON = os.getenv('CLIENT_JSON')
 
-# Length of desired clip (in seconds)
+# initializes twitter object
+twitter = Twython(AP_KY, AP_SEC, AUTH_TOK, AUTH_SEC)
+
+# initializes google cloud storage client, stores videos/songs
+storage_client = storage.Client.from_service_account_json(CL_JSON)
+
+# length of desired clip (in seconds)
 CLIP_LENGTH = 30
 
-# Name of song for the current clip
+# name of song for the current tweet
 song_name = ""
 
-# Randomly selects a video and grabs a sublcip from it
+# randomly selects a video and grabs a sublcip from it
 def get_video(list_of_docs):
     global CLIP_LENGTH
     doc_num = random.randint(0, len(list_of_docs) - 1)
@@ -30,7 +39,7 @@ def get_video(list_of_docs):
     clip = clip.resize((1280, 720))
     return clip
 
-# Randomly selects a song and grabs a snippet from it
+# randomly selects a song and grabs a snippet from it
 def get_song(list_of_songs):
     global CLIP_LENGTH
     global song_name
@@ -43,42 +52,46 @@ def get_song(list_of_songs):
     mp3 = song.subclip(start_time, start_time + CLIP_LENGTH)
     return mp3
 
-# Creates a clip by combining footage from a nature documentary with
+# creates a clip by combining footage from a nature documentary with
 # a randomly selected song
 def create_clip(list_of_songs, list_of_docs):
     mp3 = get_song(list_of_songs)
     clip = get_video(list_of_docs)
     clip = clip.set_audio(mp3)
     return clip
-    
-# Main function
-def main():
-    global song_name
-    print("Starting...")
-    song_list = os.listdir("songs/")
-    doc_list = os.listdir("docs/")
+
+# replies to the new tweet with the name of the song (and animal emoji)
+def reply_song_name():
     emoji_file = open("emoji_list.txt", "r")
     emojis = emoji_file.read().split(",")
     emoji_file.close()
-    print("Generating clip...")
-    clip = create_clip(song_list, doc_list)
-    print("Writing video...")
-    clip.write_videofile("clip.mp4")
-    # Converts audio to the codec Twitter likes using ffmpeg
-    os.system("ffmpeg -y -i clip.mp4 -c:v copy -c:a aac tweetme.mp4")
-    print("Tweeting video...")
-    video = open('tweetme.mp4', 'rb')
-    response = twitter.upload_video(media=video, media_type='video/mp4')
-    twitter.update_status(status='', media_ids=[response['media_id']])
-    print("Done! Video has been tweeted.")
-    new_tweet = twitter.get_user_timeline(user_id = ACCOUNT_ID, count_id = 1)[0]
+    new_tweet = twitter.get_user_timeline(user_id = ACNT_ID, count_id = 1)[0]
     screen_name = new_tweet['user']['screen_name']
     tweet_id = new_tweet['id']
     random_emoji = emojis[random.randint(0, len(emojis) - 1)].encode('utf-8').decode('unicode-escape')
     twitter.update_status(status = "@" + screen_name + " " + random_emoji + " song: " + song_name,
                           in_reply_to_status_id = tweet_id)
+    
+# main function
+def main():
+    global song_name
+    print("Starting...")
+    song_list = os.listdir("songs/")
+    doc_list = os.listdir("docs/")
+    print("Generating clip...")
+    clip = create_clip(song_list, doc_list)
+    print("Writing video...")
+    clip.write_videofile("clip.mp4")
+    # converts audio to the codec Twitter likes using ffmpeg
+    os.system("ffmpeg -y -i clip.mp4 -c:v copy -c:a aac tweet.mp4")
+    print("Tweeting video...")
+    video = open('tweet.mp4', 'rb')
+    response = twitter.upload_video(media=video, media_type='video/mp4')
+    twitter.update_status(status='', media_ids=[response['media_id']])
+    print("Done! Video has been tweeted.")
+    reply_song_name()
     if (os.path.exists("clip.mp4")):
         os.remove("clip.mp4")
 
-# Starts the main function
+# starts the main function
 main()
